4f4768c60a5169d7c4b56937a4231c9b93df2db1
Richa Kumari <106690186+Richa202057@users.noreply.github.com>
2023-05-08 21:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,38 +50,35 @@ os=st.selectbox('Operating System',df['os'].unique())
 
 # creating a button
 # when user will press this button this function will be executed
-if st.button('Predict Price'):
+try:
+    if st.button('Predict Price'):
+        ## input query (collecting all inputs)
+        if touchscreen == 'Yes':
+            touchscreen=1
+        else:
+            touchscreen=0
+        if ips=='Yes':
+            ips=1
+        else:
+            ips=0
 
-    ## input query (collecting all inputs)
-    if touchscreen == 'Yes':
-        touchscreen=1
-    else:
-        touchscreen=0
-    if ips=='Yes':
-        ips=1
-    else:
-        ips=0
 
+        # calculating ppi by the input of screen size and resolution
+        # resolution is taken as a string so, convert it into int for performing mathematical calculations.
+        x_reso=int(resolution.split('x')[0])
+        y_reso = int(resolution.split('x')[1])
 
-    # calculating ppi by the input of screen size and resolution
-    # resolution is taken as a string so, convert it into int for performing mathematical calculations.
-    x_reso=int(resolution.split('x')[0])
-    y_reso = int(resolution.split('x')[1])
-    
-    try:
-        ppi = ((x_reso ** 2) + (y_reso ** 2) ** 0.5) / size
-    except ZeroDivisionError:
-        print("Do not Enter Size as Zero") 
-
-    query = np.array([brand, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
-    # reshaping query (n-d array)
-    # as there are 12 columns (as input) with 1 row
-    query=query.reshape(1,12)
+        ppi = ((x_reso ** 2) + (y_reso ** 2) ** 0.5)/size
+        query = np.array([brand, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+        # reshaping query (n-d array)
+        # as there are 12 columns (as input) with 1 row
+        query=query.reshape(1,12)
 
     ## passing this input to pipe object
     ## predict() of pipe object will make prediction for this input(query).
     ## since we have applied the log transformation on output column , so now to predict the exact value we should the reverse
     ## of log operation i.e exponential of this predicted value.
-    result = int(np.exp(pipe.predict(query))[0] ) # since it is in array, so to get value without brackets I should write 0th index value of array
-    st.title("The Predicted Price of this Configuration is "+str(result))
-
+        result = int(np.exp(pipe.predict(query))[0] ) # since it is in array, so to get value without brackets I should write 0th index value of array
+        st.title("The Predicted Price of this Configuration is "+str(result)                 
+except ZeroDivisionError:
+        print("Do not Enter Size as Zero") 
